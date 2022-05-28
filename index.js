f13717bdfd64d4c3c17c78db473757e9fbba8f19
Miguel <miguel_silveira_rosa@hotmail.com>
2022-05-28 17:25:57 +0100

    Final version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Console } = require('console');
 var fs = require('fs');
 var Midi = require('jsmidgen');
 const { exit } = require('process');
+const { isNumberObject } = require('util/types');
 const { takeCoverage } = require('v8');
 
 function getMax(arr) {
@@ -14,20 +15,44 @@ function getMax(arr) {
   return max;
 }
 
+function getMax2(arr) {
+  let max = -99999;
+
+  for(let i = 0; i != arr.length; i++)
+    if(arr[i] > max)
+      max = arr[i]
+
+  return max;
+}
+
+
 function getMin(arr) {
   let len = arr.length;
   let min = 999999;
 
   while (len--) {
-      max = arr[len] < min ? arr[len] : min;
+      min = arr[len] < min ? arr[len] : min;
   }
   return min;
+}
+
+function getMin2(arr) {
+  let max = 99999;
+
+  for(let i = 0; i != arr.length; i++)
+    if(arr[i] < max)
+      max = arr[i]
+
+  return max;
 }
 
 
 const args = process.argv.slice(2)
 
 let outFimeName;
+
+let minPercentageData;
+let maxPercentageData;
 
 let var1FileName;
 let var2FileName;
@@ -36,32 +61,27 @@ let var3FileName;
 let dataFileName;
 
 //parsing args
-if (args.length == 0) {
-  console.log("No file data selected, switching to the example data file")
-  dataFileName = 'exemplo.txt';
-  outFimeName = "test"
+if (args.length == 0 || args.length == 1 || args.length == 2 || args.length == 3) {
+  console.log("Incorrect use of program");
+  console.log("The correct use of this program is the following:");
+  console.log("npm start OUTFILENAME MINPERCENTAGE MAXPERCENTAGE VAR1FILENAME [VAR2FILENAME [VER3FILENAME]]");
+  exit();
 }
 
-if (args.length == 1) {
-  outFimeName = "test"
-  dataFileName = args[0]
-}
-if (args.length == 2) {
-  outFimeName = args[0]
-  dataFileName = args[1]
-}
-if (args.length == 3) {
-  outFimeName = args[0]
-  var1FileName = args[1]
-  var2FileName = args[2]
-}
-if (args.length == 4) {
-  outFimeName = args[0]
-  var1FileName = args[1]
-  var2FileName = args[2]
-  var3FileName = args[3]
-}
+console.log(args);
+console.log(args[0]);
+console.log(args.length);
 
+if (args.length >= 4) {
+  outFimeName = args[0]
+  minPercentageData = args[1];
+  maxPercentageData = args[2];
+  var1FileName = args[3];
+  if(args.length >= 5)
+    var2FileName = args[4]
+  if(args.length >= 6)  
+    var3FileName = args[5]
+}
 
 try {
 
@@ -69,45 +89,50 @@ try {
   let var2 = [];
   let var3 = [];
 
+  console.log(outFimeName);
+  console.log(minPercentageData);
+  console.log(maxPercentageData);
+  console.log(var1FileName);
+  console.log(var2FileName);
+  console.log(var3FileName);
+
+
   //RETRIVE data
-  if (dataFileName != null) {
+  if (var1FileName != null && var2FileName == null && var3FileName == null) {
 
-    //console.log("ola")
-
-    const data = fs.readFileSync(dataFileName, 'utf8')
-
-    //console.log("3");
-
-    //console.log(data);
+    const data = fs.readFileSync(var1FileName, 'utf8')
 
     let dataFromFile = data.split("\r\n")
 
-    dataFromFile.pop();
+    //Delete the last element that is ""
+    if(dataFromFile[dataFromFile.length-1] == "")
+      dataFromFile.pop();
 
     for (let i = 0; i != dataFromFile.length; i++) {
       var1.push(Math.round(parseFloat(dataFromFile[i])))
-      var2.push(1)
+      var2.push(Math.round(parseFloat(dataFromFile[i])))
       var3.push(1)
     }
   }
 
   if (var1FileName != null && var2FileName != null && var3FileName == null) {
+    
     const data = fs.readFileSync(var1FileName, 'utf8')
     const data2 = fs.readFileSync(var2FileName, 'utf8')
-
-    //console.log(data)
-    //console.log(data2)
 
     let dataFromFile = data.split("\r\n");
     let dataFromFile2 = data2.split("\r\n");
 
-    dataFromFile.pop();
-    dataFromFile2.pop();
+    if(dataFromFile[dataFromFile.length-1] == "")
+      dataFromFile.pop();
+    if(dataFromFile2[dataFromFile2.length-1] == "")
+      dataFromFile2.pop();
 
     if (dataFromFile.length != dataFromFile2.length) {
       console.log("Files must be from the same dataset!")
       exit;
     }
+
     for (let i = 0; i != dataFromFile.length; i++) {
       var1.push(Math.round(parseFloat(dataFromFile[i])))
       var2.push(Math.round(parseFloat(dataFromFile2[i])))
@@ -124,9 +149,12 @@ try {
     let dataFromFile2 = data2.split("\r\n");
     let dataFromFile3 = data3.split("\r\n");
 
-    dataFromFile.pop();
-    dataFromFile2.pop();
-    dataFromFile3.pop();
+    if(dataFromFile[dataFromFile.length-1] == "")
+      dataFromFile.pop();
+    if(dataFromFile2[dataFromFile2.length-1] == "")
+      dataFromFile2.pop();
+    if(dataFromFile3[dataFromFile3.length-1] == "")
+      dataFromFile3.pop();
 
     if (dataFromFile.length != dataFromFile2.length || dataFromFile2.length != dataFromFile3.length) {
       console.log("Files must be from the same dataset!")
@@ -143,10 +171,33 @@ try {
     exit;
   }
 
-  //DATA normalization
-  console.log(var1.length);
-  console.log(var2.length);
+  //DATA "ZOMM"
+  let deltaPercentage = (var1.length-1)/100;
 
+  let minPointer = Math.floor(parseFloat(minPercentageData) * deltaPercentage);
+  let maxPointer = Math.floor(parseFloat(maxPercentageData) * deltaPercentage);
+
+  let aux1 = []
+  let aux2 = []
+  let aux3 = []
+
+  for(let i = 0; i != var1.length; i++) {
+    if(i >= minPointer && i <= maxPointer) {
+      aux1.push(var1[i]);
+      aux2.push(var2[i]);
+      aux3.push(var3[i]);
+    }
+  }
+
+  var1 = aux1;
+  var2 = aux2;
+  var3 = aux3;
+
+  console.log(var1)
+  console.log(var2)
+  console.log(var3)
+
+  //DATA normalization  
   let var1min = getMin(var1);
   let var2min = getMin(var2);
   let var3min = getMin(var3);
@@ -156,9 +207,9 @@ try {
   let var3mean = getMax(var3) - var3min;
 
   //console.log(var1max)
-  console.log(var1min)
-  console.log(var1mean)
-  console.log(var1)
+  //console.log(var1min)
+  //console.log(var1mean)
+  //console.log(var1)
 
   for (let i = 0; i != var1.length; i++) {
     if ((var1[i] - var1min) != 0)
